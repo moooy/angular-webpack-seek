eace3e3103b0855288b4df5c535848f25773b067
moooy <cfc_blue@foxmail.com>
2016-01-29 01:14:45 +0800

    first kiff

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path =require('path');
+
+module.exports = {
+    entry: "./src/app.ts",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "bundle.js"
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['', '.webpack.js', '.web.js', '.ts',  '.js']
+    },
+    module: {
+        loaders: [
+            { test: /\.ts$/, loader: 'ts-loader' }
+        ]
+    }
+};
